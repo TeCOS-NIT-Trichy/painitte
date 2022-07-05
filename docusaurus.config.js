8dee00ac8c487612ce90ffadc3198a6e323e61c6
Akash Jaiswal <33419526+jaiakash@users.noreply.github.com>
2022-07-06 04:28:05 +0530

    fix: github links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/technical-council-nitt/painite-tc/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/technical-council-nitt/painite-tc/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/technical-council-nitt/painite-tc/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/technical-council-nitt/painite-tc/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
